11ecf1a2a56f844f1198a532fa11b057f6f1b6fb
Kate <katie.albee@gmail.com>
2018-08-13 18:54:50 -0500

    prettyness

---
+++ client/src/components/Event/event.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import './event.css';
+import Landing from './components/Quote';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Container tag="asymmetric">
+          <Row>
+            <Col>
+                  <Landing />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default App;
